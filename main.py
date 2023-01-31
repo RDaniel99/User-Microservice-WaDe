f88a1cf5f06ec04b096a9ac953d8fcad599ca72e
Rusu <rusudaniel5799@gmail.com>
2023-01-31 10:58:37 +0200

    added parser for xspf

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from flask_cors import CORS
 from flask import Flask, request, jsonify
 from flask_jwt_extended import JWTManager, create_access_token, get_jwt_identity, jwt_required
 from werkzeug.security import generate_password_hash, check_password_hash
+import xml.etree.ElementTree as ET
 
 from config import Config
 
@@ -49,13 +50,13 @@ def register():
     data = request.get_json()
 
     if 'username' not in data:
-        return jsonify({'message': 'username field is mandatory'})
+        return jsonify({'message': 'username field is mandatory'}), 400
 
     if 'password' not in data:
-        return jsonify({'message': 'password field is mandatory'})
+        return jsonify({'message': 'password field is mandatory'}), 400
 
     if 'email' not in data:
-        return jsonify({'message': 'email field is mandatory'})
+        return jsonify({'message': 'email field is mandatory'}), 400
 
     username = data['username']
     password = data['password']
@@ -65,10 +66,13 @@ def register():
     hashed_password = generate_password_hash(password, method='sha256')
 
     # Save the user to the database
-    insert_db('users', ('username', 'password', 'email'), (username, hashed_password, email))
+    try:
+        insert_db('users', ('username', 'password', 'email'), (username, hashed_password, email))
+    except:
+        return jsonify({'message': 'Something went wrong when inserting in db'}), 400
 
     # Return a success message
-    return jsonify({'message': 'User created successfully'})
+    return jsonify({'message': 'User created successfully'}), 201
 
 
 @app.route('/login', methods=['POST'])
@@ -77,10 +81,10 @@ def login():
     data = request.get_json()
 
     if 'username' not in data:
-        return jsonify({'message': 'username field is mandatory'})
+        return jsonify({'message': 'username field is mandatory'}), 400
 
     if 'password' not in data:
-        return jsonify({'message': 'password field is mandatory'})
+        return jsonify({'message': 'password field is mandatory'}), 400
 
     username = data['username']
     password = data['password']
@@ -94,10 +98,10 @@ def login():
             # Create a JWT token
             access_token = create_access_token(identity=username)
             # Return the token
-            return jsonify({'message': 'Login successful', 'access_token': access_token})
+            return jsonify({'message': 'Login successful', 'access_token': access_token}), 200
 
         # If the username or password is incorrect, return an error message
-    return jsonify({'error': 'Invalid username or password'})
+    return jsonify({'error': 'Invalid username or password'}), 400
 
 
 @app.route('/edit', methods=['POST'])
@@ -132,13 +136,13 @@ def edit():
 
     if not added:
         conn.close()
-        return jsonify({'message': 'At least one field should be updated'})
+        return jsonify({'message': 'At least one field should be updated'}), 400
 
     conn.commit()
     conn.close()
 
     # Return a success message
-    return jsonify({'message': 'User updated successfully'})
+    return jsonify({'message': 'User updated successfully'}), 200
 
 
 @app.route('/profile/<username>', methods=['POST'])
@@ -148,19 +152,19 @@ def profile(username):
     user_to_display = query_db('SELECT * FROM users WHERE username = ?', [username], one=True)
 
     if user_to_display is None:
-        return jsonify({'message': 'There is no user with this username'})
+        return jsonify({'message': 'There is no user with this username'}), 404
 
     if username != current_user:
         return jsonify({'userData': {
             'email': user_to_display['email'],
             'username': user_to_display['username']
-        }})
+        }}), 200
 
     return jsonify({'userData': {
         'email': user_to_display['email'],
         'username': user_to_display['username'],
         'discogs_token': user_to_display['discogs_token']
-    }})
+    }}), 200
 
 
 @app.route('/playlists', methods=['POST'])
@@ -168,7 +172,7 @@ def profile(username):
 def create_playlist():
     current_user = get_jwt_identity()
     playlist_content = request.data.decode()
-    insert_db('playlists', ('playlist_content', 'user_id',), (playlist_content, current_user, ))
+    insert_db('playlists', ('playlist_content', 'user_id',), (playlist_content, current_user,))
     return 'Playlist created', 201
 
 
@@ -182,6 +186,56 @@ def get_playlist(playlist_id):
         return playlist['playlist_content'], 200
     else:
         return 'Playlist not found', 404
+
+
+@app.route('/playlists/<int:playlist_id>/info', methods=['GET'])
+@jwt_required()
+def get_playlist_info(playlist_id):
+    current_user = get_jwt_identity()
+    playlist = query_db('SELECT playlist_content FROM playlists WHERE playlist_id = ? AND user_id = ?',
+                        (playlist_id, current_user), one=True)
+
+    if not playlist:
+        return 'Playlist not found', 404
+
+    root = ET.fromstring(playlist['playlist_content'])
+
+    if not root.tag.__contains__('playlist'):
+        return jsonify({'message': 'Something went wrong when parsing your playlist...'}), 400
+
+    if not list(root)[0].tag.__contains__('trackList'):
+        return jsonify({'message': 'Something went wrong when parsing your playlist...'}), 400
+
+    # Extract information from each track in trackList
+
+    tracks = []
+    for track in list(list(root)[0]):
+        if not track.tag.__contains__('track'):
+            return jsonify({'message': 'Something went wrong when parsing your playlist...'}), 400
+
+        json = {}
+
+        for attrib in list(track):
+            if attrib.tag.__contains__('location'):
+                json['location'] = attrib.text
+            if attrib.tag.__contains__('creator'):
+                json['creator'] = attrib.text
+            if attrib.tag.__contains__('album'):
+                json['album'] = attrib.text
+            if attrib.tag.__contains__('title'):
+                json['title'] = attrib.text
+            if attrib.tag.__contains__('annotation'):
+                json['annotation'] = attrib.text
+            if attrib.tag.__contains__('duration'):
+                json['duration'] = attrib.text
+            if attrib.tag.__contains__('image'):
+                json['image'] = attrib.text
+            if attrib.tag.__contains__('info'):
+                json['info'] = attrib.text
+
+        tracks.append(json)
+
+    return jsonify({'message': tracks}), 200
 
 
 if __name__ == '__main__':
