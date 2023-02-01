dc241717facf944ae1acfaea7f64d08c16059f31
Daniel Rusu <rusudaniel5799@gmail.com>
2023-02-01 12:13:09 +0200

    revert changes (give up on life)

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
-CORS(app, resources={r"/*": {"origins": "*", "methods": "*", "headers": "*"}})
+CORS(app)
 
 # Set up Flask-JWT
 jwt = JWTManager(app)
